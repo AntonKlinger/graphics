16703d0bfaa0d344104eae81d062c8285ac2bcb5
Anton <anton.klinger@web.de>
2023-10-14 17:10:08 +0200

    add graphs

--- supply_curve/standard.py
+++ supply_curve/standard.py
@@ -7,16 +7,13 @@ x = np.linspace(0, 5, 100)  # Erzeugt 100 Werte von 0 bis 5
 # Erstellen Sie eine Funktion y = f(x), z.B. y = x^2
 y = x**2
 
-# Erstellen Sie ein Diagramm
-plt.plot(x, y)
+# Erstellen Sie ein Diagramm für die erste Funktion (Standardfarbe und Linienstil)
+plt.plot(x, y, color='blue', label='supply curve')
 
 # Beschriftungen und Titel anpassen
-plt.xlabel('quantity', color='white')
-plt.ylabel('price', color='white')
-plt.title('supply curve', color='white')  # Verwenden von LaTeX für mathematische Symbole
-
-# Beschriftung der Funktion hinzufügen
-plt.text(5, 23, '$S$', color='white', fontsize=12)  # Position (2, 10) und Text '$y = x^2'
+plt.xlabel('Menge', color='white')
+plt.ylabel('Preis', color='white')
+plt.title('Angebotskurve', color='white')  # Verwenden von LaTeX für mathematische Symbole
 
 # Achsenfarben anpassen
 ax = plt.gca()
@@ -25,12 +22,18 @@ ax.spines['top'].set_color('white')
 ax.spines['right'].set_color('white')
 ax.spines['left'].set_color('white')
 
+plt.xlim(0, 6)  # X-Achse von 0 bis 5
+plt.ylim(0, 25)  # Y-Achse von 0 bis 25
+
 # Achsenticks ausblenden
 ax.set_xticks([])
 ax.set_yticks([])
 
+# Legende hinzufügen
+plt.legend()
+
 # Das Diagramm als PNG-Datei mit transparentem Hintergrund speichern
-plt.savefig('supply_curve.png', transparent=True)
+plt.savefig('supply_curve/standard.png', transparent=True)
 
 # Das Diagramm anzeigen (optional, wenn Sie es sehen möchten)
 # plt.show()
---
+++ supply_curve/to_the_right.py
@@ -0,0 +1,50 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Erstellen Sie ein Array von x-Werten
+x = np.linspace(0, 5, 100)  # Erzeugt 100 Werte von 0 bis 5
+
+# Erstellen Sie eine Funktion y = f(x), z.B. y = x^2
+y = x**2
+
+# Erstellen Sie ein Diagramm für die erste Funktion (Standardfarbe und Linienstil)
+plt.plot(x, y, color='blue', label='original curve')
+
+x = np.linspace(0, 6, 100)  # Erzeugt 100 Werte von 0 bis 5
+
+# Erstellen Sie eine Funktion y = f(x), z.B. y = x^2
+y = 0.8*x**2 - 2
+
+# Erstellen Sie ein Diagramm für die zweite Funktion mit spezifischer Farbe und gestricheltem Linienstil
+plt.plot(x, y, color='blue', linestyle='--', label='new curve')
+
+# Beschriftungen und Titel anpassen
+plt.xlabel('Menge', color='white')
+plt.ylabel('Preis', color='white')
+plt.title('Angebotskurve', color='white')  # Verwenden von LaTeX für mathematische Symbole
+
+# Achsenfarben anpassen
+ax = plt.gca()
+ax.spines['bottom'].set_color('white')
+ax.spines['top'].set_color('white')
+ax.spines['right'].set_color('white')
+ax.spines['left'].set_color('white')
+
+plt.xlim(0, 6)  # X-Achse von 0 bis 5
+plt.ylim(0, 25)  # Y-Achse von 0 bis 25
+
+# Achsenticks ausblenden
+ax.set_xticks([])
+ax.set_yticks([])
+
+# Legende hinzufügen
+plt.legend()
+
+# Das Diagramm als PNG-Datei mit transparentem Hintergrund speichern
+plt.savefig('supply_curve/to_the_right.png', transparent=True)
+
+# Das Diagramm anzeigen (optional, wenn Sie es sehen möchten)
+# plt.show()
+
+# Schließen Sie das Diagramm
+plt.close()
